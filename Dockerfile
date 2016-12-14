c484f08aaaa2aafd397c0880d54a4200f160c913
ssstrz <codecats@gmail.com>
2016-12-14 22:21:34 +0100

    added logger

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,20 @@
-FROM python:2.7
+FROM ubuntu:latest
 
-ADD . /code
+ADD . /code/src
 WORKDIR /code
 
-RUN bash -c "virtualenv venv && . venv/bin/activate && pip install -r requirements/main.txt"
+RUN apt-get -qqy update
+RUN apt-get -qqy upgrade
+RUN apt-get -qqy install \
+    python \
+    python-dev \
+    python-gtk2 \
+    python-pip \
+    python-wxgtk3.0 \
+    winpdb
+
+RUN pip install virtualenv
+
+RUN bash -c "virtualenv venv && . venv/bin/activate && pip install -r src/requirements/main.txt"
+
+CMD ['venv/bin/python', '-u', 'src/main.py', "1>server.log", "2>server.log"]
--- main.py
+++ main.py
@@ -1,20 +1,51 @@
 import os
-import tornado.httpserver
-import tornado.ioloop
-import tornado.web
- 
-class MainHandler(tornado.web.RequestHandler):
+import sys
+from tornado import ioloop, web, websocket
+import logging
+
+logging.basicConfig(filename='logs/main.log',level=logging.DEBUG)
+
+
+cl = []
+
+class SocketHandler(websocket.WebSocketHandler):
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        logging.debug(self)
+        cl.append(self)
+
+    def on_close(self):
+        if self in cl:
+            cl.remove(self)
+
+
+class ApiHandler(web.RequestHandler):
+
+    @web.asynchronous
+    def get(self, *args):
+        self.finish()
+        for c in cl:
+            logging.debug(c)
+            c.write_message('{"msg": "hello"}')
+
+    @web.asynchronous
+    def post(self):
+        pass
+
+
+class MainHandler(web.RequestHandler):
     def get(self):
         self.write("Hello world")
  
-def main():
-    application = tornado.web.Application([
-        (r"/", MainHandler),
-    ])
-    http_server = tornado.httpserver.HTTPServer(application)
-    port = int(os.environ.get("PORT", 5000))
-    http_server.listen(port)
-    tornado.ioloop.IOLoop.instance().start()
+app = web.Application([
+    (r'/', MainHandler),
+    (r'/ws', SocketHandler),
+    (r'/api', ApiHandler),
+])
+
  
 if __name__ == "__main__":
-    main()
+    app.listen(int(os.environ.get("PORT", 5000)))
+    ioloop.IOLoop.instance().start()
